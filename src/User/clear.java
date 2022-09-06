39e6a46b5c3dd7025dce0cad4ba981cd65359488
Lysyndre <112764236+Lysyndre@users.noreply.github.com>
2022-09-06 23:22:52 +0300

    Delete clear.java

--- src/User/clear.java
+++
@@ -1,9 +0,0 @@
-package User;
-import java.io.IOException;
-public class clear {
-    public static void main(String[] args) throws IOException{
-        System.out.print("Everything on the console will cleared");
-        Runtime.getRuntime().exec("cls");
-        //System.out.print("\033[H\033[2J");
-        //System.out.flush();
-}}
